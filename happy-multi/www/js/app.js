646e1cdd04a6821004d9e3b8780ba184f938c298
Cédric <cedced19@gmail.com>
2015-11-04 21:45:42 +0100

    Create tags, init navigations, and others

---
+++ happy-multi/www/js/app.js
@@ -0,0 +1,16 @@
+﻿phonon.options({
+    navigator: {
+        defaultPage: 'home',
+        animatePages: true,
+        enableBrowserBackButton: true
+    },
+    i18n: { 
+            directory: 'langs/',
+            localeFallback: 'en'
+    }
+});
+
+// phonon.updateLocale('en'); to test in english
+
+phonon.i18n().bind();
+phonon.navigator().start();
