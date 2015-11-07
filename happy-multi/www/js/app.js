d8e8620b62f87b2dbc11625480ee6f7ed6d5bf81
Cédric <cedced19@gmail.com>
2015-11-07 10:16:17 +0100

    Allow to update language

--- happy-multi/www/js/app.js
+++ happy-multi/www/js/app.js
@@ -10,7 +10,41 @@
     }
 });
 
-// phonon.updateLocale('en'); to test in english
+var language = localStorage.getItem('language') || (window.navigator.userLanguage || window.navigator.language).split('-')[0];
+phonon.updateLocale(language);
+
+phonon.navigator().on({page: 'home', preventClose: false, readyDelay: 0}, function(activity) {
+
+    activity.onCreate(function() {
+        
+    });
+});
+
+phonon.navigator().on({page: 'language', preventClose: false, readyDelay: 0}, function (activity) {
+    var radios = document.getElementsByName('language');
+
+    activity.onCreate(function () {
+        document.querySelector('#language-btn').on('click', function () {
+            for (var i = 0, length = radios.length; i < length; i++) {
+                if (radios[i].checked) {
+                    localStorage.setItem('language', radios[i].value);
+                    phonon.updateLocale(radios[i].value);
+                    language = radios[i].value;
+                    break;
+                }
+            }
+        });
+    });
+
+    activity.onReady(function () {
+        for (var i = 0, length = radios.length; i < length; i++) {
+            if (radios[i].value == language) {
+                radios[i].checked = true;
+                break;
+            }
+        }
+    });
+});
 
 phonon.i18n().bind();
 phonon.navigator().start();
